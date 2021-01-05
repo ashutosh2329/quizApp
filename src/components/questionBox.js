92500d276ed29c3e245239a63473aeedc2ca8c5a
ashutosh <sonutiwari2329@gmail.com>
2021-01-06 00:34:49 +0530

    color fixing by id

--- src/components/questionBox.js
+++ src/components/questionBox.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import { Card, Container, Paper, Typography } from "@material-ui/core";
 
-const Option = ({ data }) => {
+const Option = ({ data, id }) => {
   return (
     <Typography
       variant="h6"
       color="textSecondary"
       style={{ height: 50, padding: 10, marginTop: 10 }}
+      id={id}
     >
       {data}
     </Typography>
@@ -22,10 +23,13 @@ export default function QuestionBox({ question, counter, setResult }) {
   const handleClick = (value) => {
     if (!clicked) {
       setClicked(true);
-      if (answer === value.slice(3)) {
+      var element = document.getElementById(value.id);
+      if (answer === value.textContent.slice(3)) {
         setResult(1);
+        element.style.backgroundColor = "Lime";
       } else {
         setResult(0);
+        element.style.backgroundColor = "red";
       }
     }
   };
@@ -41,18 +45,18 @@ export default function QuestionBox({ question, counter, setResult }) {
         {counter}.{question.Question}
       </Typography>
       <div>
-        <div onClick={(e) => handleClick(e.target.textContent)}>
-          <Option data={`a. ${question.options[0]}`} />
+        <div onClick={(e) => handleClick(e.target)}>
+          <Option id="A" data={`a. ${question.options[0]}`} />
         </div>
-        <div onClick={(e) => handleClick(e.target.textContent)}>
-          <Option data={`b. ${question.options[1]}`} />
+        <div onClick={(e) => handleClick(e.target)}>
+          <Option id="B" data={`b. ${question.options[1]}`} />
         </div>
-        <div onClick={(e) => handleClick(e.target.textContent)}>
-          <Option data={`c. ${question.options[2]}`} />
+        <div onClick={(e) => handleClick(e.target)}>
+          <Option id="C" data={`c. ${question.options[2]}`} />
         </div>
 
-        <div onClick={(e) => handleClick(e.target.textContent)}>
-          <Option data={`d. ${question.options[3]}`} />
+        <div onClick={(e) => handleClick(e.target)}>
+          <Option id="D" data={`d. ${question.options[3]}`} />
         </div>
       </div>
     </Paper>
